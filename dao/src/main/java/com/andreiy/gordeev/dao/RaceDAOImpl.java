f4494024470fb64d8a64dcce30819d3f81247ac1
agordeev <andreiy.gordeev@tech-stack.io>
2018-09-24 19:29:16 +0300

    Removed model module. Added random number for race, for test purposes.

--- dao/src/main/java/com/andreiy/gordeev/dao/RaceDAOImpl.java
+++ dao/src/main/java/com/andreiy/gordeev/dao/RaceDAOImpl.java
@@ -3,12 +3,19 @@ package com.andreiy.gordeev.dao;
 import com.andreiy.gordeev.model.Race;
 import org.springframework.stereotype.Repository;
 
+import java.util.Random;
+
 @Repository
 public class RaceDAOImpl implements RaceDAO {
 
     public Race getRace() {
         Race race = new Race();
-        race.setRaceName("Human");
+        race.setRaceName("Human " + getRandomNumber());
         return race;
+    }
+
+    public int getRandomNumber() {
+        Random rand = new Random();
+        return rand.nextInt(100);
     }
 }
