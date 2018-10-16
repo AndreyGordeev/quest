67917f2e44fb9c0ea40c2408a404f724ece35529
agordeev <andreiy.gordeev@tech-stack.io>
2018-10-16 12:44:40 +0300

    Database properties added.

--- dao/src/main/java/com/andreiy/gordeev/config/DbConfig.java
+++ dao/src/main/java/com/andreiy/gordeev/config/DbConfig.java
@@ -1,23 +1,38 @@
 package com.andreiy.gordeev.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.andreiy.gordeev")
+@PropertySource("classpath:application.properties")
 public class DbConfig {
+
+    @Value("${database.driver}")
+    private String dbDriver;
+
+    @Value("${database.url}")
+    private String dbUrl;
+
+    @Value("${database.username}")
+    private String dbUserName;
+
+    @Value("${database.password}")
+    private String dbUserPass;
 
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:12345/quest");
-        dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setDriverClassName(dbDriver);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUserName);
+        dataSource.setPassword(dbUserPass);
 
         return dataSource;
     }
